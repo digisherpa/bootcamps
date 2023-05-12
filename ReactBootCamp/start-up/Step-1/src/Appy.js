75348a211df183ef3d18151ee57ea21f7758f4ed
Nidhi Parmar <nidhi@OnkarMBP.local>
2023-05-12 13:29:04 +0200

    step-0

---
+++ ReactBootCamp/start-up/Step-1/src/Appy.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Appy extends Component {
+  render() {
+    return (
+      <div className="Appy">
+        <h1>Hello, React!</h1>
+      </div>
+    )
+  }
+}
+
+export default Appy
