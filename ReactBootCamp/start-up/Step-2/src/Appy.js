4cda4f8c56bdd1f51a9e3dc172dc268c5d146b2f
Nidhi Parmar <nidhi@OnkarMBP.local>
2023-05-12 15:31:14 +0200

    Step-2

---
+++ ReactBootCamp/start-up/Step-2/src/Appy.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Table from './Table'
+
+class Appy extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Table />
+      </div>
+    )
+  }
+}
+
+export default Appy
